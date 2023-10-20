ffbb1eda89436a006afe12c73d37bdc1752447e2
MaxSt97 <105374626+MaxSt97@users.noreply.github.com>
2023-10-20 11:06:50 +0200

    All of the components are working atm. porducer -> broker -> consumer -> postgres raw_table

--- consumer.py
+++ consumer.py
@@ -1,9 +1,7 @@
-
 from kafka import KafkaConsumer
 import psycopg2
 
 # Kafka-Consumer-Konfiguration
-
 consumer = KafkaConsumer(
     'topicxx',
     bootstrap_servers=['broker:9092'],
@@ -11,40 +9,57 @@ consumer = KafkaConsumer(
     consumer_timeout_ms=2000)
 
 
+table_name = "raw_data"
+
+
+# Header-Reihe aus der ersten Kafka-Nachricht lesen
 header_row = next(consumer)
 header_row = header_row.value.decode('utf-8').split(',')
-
 
 try:
     conn = psycopg2.connect(
         host="raw_data_db",
-        database="raw_data",  # Ändern Sie den Datenbanknamen entsprechend
+        database="raw_data",
         user="my_username",
         password="my_password",
         port="5432"
     )
     cursor = conn.cursor()
     print("Verbindung zur PostgreSQL-Datenbank erfolgreich hergestellt.")
-    # Erstellen Sie eine neue Tabelle mit den Überschriften aus header_row
-    create_table_query = f"CREATE TABLE IF NOT EXISTS raw_data ({', '.join(header_row)} VARCHAR)"
-    cursor.execute(create_table_query)
+
+
+
+    columns_and_types = ", ".join([f"{col} text" for col in header_row])
+
+    # Erstellen Sie die Tabelle mit den Spalten und Datentypen
+    create_table_sql = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_and_types})"
+    cursor.execute(create_table_sql)
     conn.commit()
 
 
+
+
+    for message in consumer:
+        # Nachrichtenwerte in eine Liste umwandeln
+        data_values = message.value.decode('utf-8').split(',')
+
+        # Erstellen Sie die INSERT INTO-Anweisung dynamisch
+        insert_query = f"INSERT INTO raw_data ({', '.join(header_row)}) VALUES ({', '.join(['%s'] * len(header_row))})"
+
+        # Führen Sie den INSERT-Befehl aus
+        cursor.execute(insert_query, data_values)
+        conn.commit()
+
+    print("Tabelle erfolgreich erstellt und Daten eingefügt.")
+
 except Exception as error:
-    print(f"Fehler bei der Verbindung zur Datenbank: {error}")
+    print(f"Fehler bei der Verbindung zur Datenbank oder beim Erstellen der Tabelle: {error}")
 finally:
     # Verbindung und Cursor schließen
     if cursor:
         cursor.close()
     if conn:
         conn.close()
-
-for message in consumer:
-    data = message.value.decode('utf-8')
-    values = data.split(',')
-
-
 
 # Verbindung schließen
 consumer.close()
